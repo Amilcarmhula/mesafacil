338dd490bce183dc823dc39b403002c9726b28a5
Amilcar Mula <amilcarmhula.am@gmail.com>
2024-06-11 12:53:05 +0200

    update backoffice and client

---
+++ backoffice/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import logo from './logo.svg';
+import { Counter } from './features/counter/Counter';
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <h1>Ola Mundo</h1>
+    </div>
+  );
+}
+
+export default App;
